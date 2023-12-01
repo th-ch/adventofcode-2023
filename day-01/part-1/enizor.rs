4c88f0f43c1904337cd15f8875036b26bed94526
Rémi Garde <rge7@3ds.com>
2023-12-01 14:37:05 +0100

    Enizor Day 1

---
+++ day-01/part-1/enizor.rs
@@ -0,0 +1,42 @@
+use std::env::args;
+use std::time::Instant;
+
+fn main() {
+    let now = Instant::now();
+    let output = run(&args().nth(1).expect("Please provide an input"));
+    let elapsed = now.elapsed();
+    println!("_duration:{}", elapsed.as_secs_f64() * 1000.);
+    println!("{}", output);
+}
+
+fn run(input: &str) -> isize {
+    let mut res = 0;
+    for line in input.lines() {
+        let mut first = true;
+        let mut last_val = 0;
+        for c in line.bytes() {
+            if c >= b'0' && c <= b'9' {
+                last_val = (c - b'0') as isize;
+                if first {
+                    res += 10*last_val;
+                    first = false;
+                }
+            }
+        }
+        res += last_val;
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn run_test() {
+        assert_eq!(run("1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"), 142)
+    }
+}
