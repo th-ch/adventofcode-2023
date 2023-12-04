415eba63cdc01d57f1e324ec0478e11b3ab90aaa
Antoine Delègue <delegue.antoine@gmail.com>
2023-12-04 09:37:40 +0100

    day 4

---
+++ day-04/part-2/vanyle.nim
@@ -0,0 +1,57 @@
+from times import cpuTime
+from os import paramStr
+
+import sequtils, strutils
+
+proc fparseInt(s: string, i: int): int =
+    var j = i
+    while j < s.len and not s[j].isDigit:
+        inc j
+    while j < s.len and s[j].isDigit:
+        result *= 10
+        result += cast[int](s[j]) - cast[int]('0')
+        inc j
+
+proc run(s: string): string =
+    var wTable: seq[int] = newSeq[int](s.len div 117 + 1)
+    var i = 0
+    var idx = 0
+    while i < s.len:
+        var l = s[i+9..i+115]
+
+        var sh: set[uint8]
+        for j in 0..<10:
+            let idx = 10 + 3 * j + i
+            sh.incl(fparseInt(s, idx).uint8)
+
+        var matches = 0
+
+        for j in 0..<25:
+            let idx = 42 + 3 * j + i
+            let num = fparseInt(s, idx).uint8
+            if num in sh:
+                inc matches
+
+        wTable[idx] = matches # card i makes you win 'matches' cards
+        i += 117
+        inc idx
+
+    var cardsOwned: seq[int] = newSeq[int](wTable.len)
+    for i in 0..<cardsOwned.len:
+        cardsOwned[i] = 1
+
+    for i in 0..<cardsOwned.len:
+        var w = wTable[i]
+        for j in (i+1) ..< (i + 1 + w):
+            cardsOwned[j] += cardsOwned[i]   
+
+    return $cardsOwned.foldl(a + b)
+
+
+var input: string = paramStr(1)
+
+var t0 = cpuTime()
+var output = run(input)
+
+echo "_duration:", (cpuTime() - t0) * 1000
+echo output
