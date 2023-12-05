ac47b822a79069bbad0ca0f9ca1fed6a0b66d67c
Thomas Renault <thomas.renault56@gmail.com>
2023-12-05 20:57:42 +0100

    day 5 fix

--- day-05/part-2/thomren.py
+++ day-05/part-2/thomren.py
@@ -22,10 +22,17 @@ class ThomrenSubmission(SubmissionPy):
             )
             mapped_ranges = []
             for start, end in ranges:
-                if mappings[0][0] > start:
-                    mapped_ranges.append((start, mappings[0][0]))
-                    start = mappings[0][0]
                 for source_start, dest_start, map_length in mappings:
+                    if start == end:
+                        break
+                    if start < source_start:
+                        mapped_ranges.append((start, min(source_start, end)))
+                        start = min(source_start, end)
+                    if end < source_start:
+                        mapped_ranges.append((start, end))
+                        start = end
+                        break
+
                     intersect_start, intersect_end = max(start, source_start), min(
                         end, source_start + map_length
                     )
@@ -37,8 +44,10 @@ class ThomrenSubmission(SubmissionPy):
                             )
                         )
                         start = intersect_end
+
                 if end > start:
                     mapped_ranges.append((start, end))
+
             ranges = mapped_ranges
         return min(start for (start, _) in ranges)
 
@@ -50,40 +59,40 @@ def test_thomren():
     assert (
         ThomrenSubmission().run(
             """
-seeds: 79 14 55 13
+    seeds: 79 14 55 13
 
-seed-to-soil map:
-50 98 2
-52 50 48
+    seed-to-soil map:
+    50 98 2
+    52 50 48
 
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
 
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
 
-water-to-light map:
-88 18 7
-18 25 70
+    water-to-light map:
+    88 18 7
+    18 25 70
 
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
 
-temperature-to-humidity map:
-0 69 1
-1 0 69
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
 
-humidity-to-location map:
-60 56 37
-56 93 4
-""".strip()
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """.strip()
         )
         == 46
     )
