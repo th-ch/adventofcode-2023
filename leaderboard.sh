262129f2a0059e5950f9d429a9c444ae68ed6ec5
Antoine Delègue <delegue.antoine@gmail.com>
2023-12-05 14:26:47 +0100

    feat: add uploading to netlify
    the NETLIFY_AUTH variable needs to be filled in

--- leaderboard.sh
+++ leaderboard.sh
@@ -8,3 +8,5 @@ export PATH=$PATH:~/.cargo/bin:$GOROOT/bin
 ./aoc run --all
 
 # Upload the content of leaderboard to the web.
+netlify deploy --prod --site cs-advent-of-code-2023 -d leaderboard --auth $NETLIFY_AUTH
+
