7f22993d856a15ee44506815e254abf8b673cb70
Rémi Garde <remi.garde@free.fr>
2023-12-07 22:42:05 +0100

    Enizor Day 7: optimization
    - use LUT to compute index from card letter
    - merge hand & bet into a single u64

--- day-07/part-1/enizor.rs
+++ day-07/part-1/enizor.rs
@@ -9,22 +9,30 @@ fn main() {
     println!("{}", output);
 }
 
-fn run(input: &str) -> usize {
+fn run(input: &str) -> u64 {
     let mut hands = Vec::with_capacity(1010);
-    for l in input.lines() {
-        hands.push((
-            hand_value(&l.as_bytes()[0..5]),
-            l[6..].parse::<usize>().unwrap(),
-        ));
+    let mut cur = 0;
+    let bytes = input.as_bytes();
+    while cur < bytes.len() {
+        let h_v = hand_value(&bytes[cur..cur + 5]) as u64;
+        cur += 6;
+        let mut bet = 0;
+        while cur < bytes.len() && bytes[cur].is_ascii_digit() {
+            bet *= 10;
+            bet += (bytes[cur] - b'0') as u32;
+            cur += 1;
+        }
+        cur += 1;
+        hands.push((h_v << 32) | (bet as u64));
     }
     hands.sort_unstable();
     hands
         .iter()
         .enumerate()
-        .fold(0, |acc, (i, &(_h, b))| acc + b * (i + 1))
+        .fold(0, |acc, (i, &h)| acc + (h & 0xFFFFFFFFu64) * (i as u64 + 1))
 }
 
-fn card_index(card: u8) -> u8 {
+const fn card_index(card: u8) -> u8 {
     match card {
         b'2'..=b'9' => card - b'2',
         b'T' => 8,
@@ -32,9 +40,22 @@ fn card_index(card: u8) -> u8 {
         b'Q' => 10,
         b'K' => 11,
         b'A' => 12,
-        _ => panic!("Unknown card"),
+        _ => 0,
     }
 }
+
+const fn compute_lut() -> [u8; 256] {
+    let mut res = [0; 256];
+    let mut i = 0;
+    while i < 256 {
+        res[i] = card_index(i as u8);
+        i += 1;
+    }
+    res
+}
+
+const CARD_INDEX: [u8; 256] = compute_lut();
+
 // Hand representation:
 // same card counts - with 1s and 0s for easier loop -> 12bits
 // 0b554433221100
@@ -51,7 +72,7 @@ fn hand_value(hand: &[u8]) -> u32 {
     // count each card on 4 bits
     let mut combo_count = 0u64;
     for &c in &hand[0..5] {
-        let i = card_index(c);
+        let i = CARD_INDEX[c as usize];
         card_value <<= 4;
         card_value += i as u32;
         combo_count += 1 << (4 * i)
