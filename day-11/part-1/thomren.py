b80c0e9af45dca75d7682e6d5303c64f0d7f0c0a
Thomas Renault <thomas.renault56@gmail.com>
2023-12-11 10:49:04 +0100

    day 11

---
+++ day-11/part-1/thomren.py
@@ -0,0 +1,52 @@
+from itertools import product
+import numpy as np
+from tool.runners.python import SubmissionPy
+
+
+class ThomrenSubmission(SubmissionPy):
+    def run(self, s: str):
+        """
+        :param s: input in string format
+        :return: solution flag
+        """
+        grid = np.array(
+            [[1 if c == "#" else 0 for c in line] for line in s.splitlines()]
+        )
+        y_offsets = ((grid.sum(axis=0) == 0)).cumsum()
+        x_offsets = ((grid.sum(axis=1) == 0)).cumsum()
+        xs, ys = np.nonzero(grid)
+        xs += x_offsets[xs]
+        ys += y_offsets[ys]
+        galaxies = list(zip(xs, ys))
+
+        sum_distances = 0
+        for i in range(len(galaxies)):
+            for j in range(i + 1, len(galaxies)):
+                dist = abs(galaxies[i][0] - galaxies[j][0]) + abs(
+                    galaxies[i][1] - galaxies[j][1]
+                )
+                sum_distances += dist
+        return sum_distances
+
+
+def test_thomren():
+    """
+    Run `python -m pytest ./day-11/part-1/thomren.py` to test the submission.
+    """
+    assert (
+        ThomrenSubmission().run(
+            """
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+""".strip()
+        )
+        == 374
+    )
