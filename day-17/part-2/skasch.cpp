514bb0771a47b4c8bd34a21ab8b869d59c54617f
skasch <romain.mondoncancel@gmail.com>
2023-12-17 22:10:12 +0100

    Use trick to simplify logic

--- day-17/part-2/skasch.cpp
+++ day-17/part-2/skasch.cpp
@@ -9,7 +9,9 @@
 
 static constexpr int kSize = 141;
 static constexpr int kInputSize = kSize * (kSize + 1) - 1;
-static std::vector<bool> kVisited(kInputSize * 4 * 10, false);
+static constexpr int kMinSteps = 4;
+static constexpr int kMaxSteps = 10;
+static std::vector<bool> kVisited(kInputSize * 4, false);
 static constexpr int kRight = 1;
 static constexpr int kLeft = -1;
 static constexpr int kDown = kSize + 1;
@@ -45,48 +47,17 @@ int EnumToDir(int index) {
   }
 }
 
-int StateToIndex(int pos, int direction, int steps) {
-  return kInputSize * 4 * (steps - 1) + kInputSize * DirToEnum(direction) + pos;
+int StateToIndex(int pos, int direction) {
+  return kInputSize * DirToEnum(direction) + pos;
 }
 
 int IndexToPos(int index) { return index % kInputSize; }
-int IndexToDir(int index) { return EnumToDir((index / kInputSize) % 4); }
-int IndexToSteps(int index) { return index / (4 * kInputSize) + 1; }
+int IndexToDir(int index) { return EnumToDir(index / kInputSize); }
 
 int HeatLoss(char c) { return c - '0'; }
 
 bool IsValid(int pos) {
   return pos >= 0 && pos < kInputSize && pos % (kSize + 1) != kSize;
-}
-
-std::vector<int> ValidDirections(int direction, int steps) {
-  if (steps <= 3) {
-    return {direction};
-  }
-  if (steps < 10) {
-    switch (direction) {
-      case kRight:
-        return {kRight, kUp, kDown};
-      case kLeft:
-        return {kLeft, kUp, kDown};
-      case kUp:
-        return {kUp, kLeft, kRight};
-      case kDown:
-        return {kDown, kLeft, kRight};
-      default:
-        throw std::invalid_argument("Invalid direction.");
-    }
-  }
-  switch (direction) {
-    case kRight:
-    case kLeft:
-      return {kUp, kDown};
-    case kUp:
-    case kDown:
-      return {kRight, kLeft};
-    default:
-      throw std::invalid_argument("Invalid direction.");
-  }
 }
 
 using PQ =
@@ -95,25 +66,41 @@ using PQ =
 
 int FindShortest(const std::string& input) {
   PQ pq;
-  pq.push({HeatLoss(input[1]), StateToIndex(1, kRight, 1)});
-  pq.push({HeatLoss(input[kSize + 1]), StateToIndex(kSize + 1, kDown, 1)});
+  pq.push({0, StateToIndex(0, kRight)});
+  pq.push({0, StateToIndex(0, kDown)});
   while (!pq.empty()) {
     auto [heat, index] = pq.top();
     pq.pop();
     int pos = IndexToPos(index);
+    if (pos == kInputSize - 1) {
+      return heat;
+    }
     if (kVisited[index]) continue;
     kVisited[index] = true;
     int direction = IndexToDir(index);
-    int steps = IndexToSteps(index);
-    for (int next_dir : ValidDirections(direction, steps)) {
-      int next_pos = pos + next_dir;
-      if (!IsValid(next_pos)) continue;
-      int next_heat = heat + HeatLoss(input[next_pos]);
-      int next_steps = (next_dir == direction) ? steps + 1 : 1;
-      if (next_pos == kInputSize - 1 && next_steps >= 4) {
-        return next_heat;
+    int next_pos = pos;
+    int next_heat = heat;
+    for (int steps = 1; steps <= kMaxSteps; ++steps) {
+      next_pos += direction;
+      if (!IsValid(next_pos)) break;
+      next_heat += HeatLoss(input[next_pos]);
+      if (steps < kMinSteps) continue;
+      switch (direction) {
+        case kLeft:
+        case kRight: {
+          pq.push({next_heat, StateToIndex(next_pos, kUp)});
+          pq.push({next_heat, StateToIndex(next_pos, kDown)});
+          break;
+        }
+        case kUp:
+        case kDown: {
+          pq.push({next_heat, StateToIndex(next_pos, kRight)});
+          pq.push({next_heat, StateToIndex(next_pos, kLeft)});
+          break;
+        }
+        default:
+          throw std::invalid_argument("Invalid direction");
       }
-      pq.push({next_heat, StateToIndex(next_pos, next_dir, next_steps)});
     }
   }
   throw std::invalid_argument("No path found.");
