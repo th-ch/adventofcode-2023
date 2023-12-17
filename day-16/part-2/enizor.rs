e2c447f0cd85ca6d770a5b2e97963a9f7c59a73b
Rémi Garde <remi.garde@free.fr>
2023-12-17 09:54:22 +0100

    setup lib for walkable grid

--- day-16/part-2/enizor.rs
+++ day-16/part-2/enizor.rs
@@ -9,65 +9,37 @@ fn main() {
     println!("{}", output);
 }
 
-#[repr(u8)]
-#[derive(Debug, Clone, Copy)]
-enum Direction {
-    Left = 0b1,
-    Right = 0b10,
-    Up = 0b100,
-    Down = 0b1000,
-}
+use aoc::enizor::grid::*;
 use Direction::*;
 
+#[derive(Debug, Clone)]
 struct Contraption<'a> {
-    grid: &'a [u8],
     grid_beams: Vec<u8>,
-    width: usize,
-    height: usize,
+    grid: StrGrid<'a>,
 }
 
 impl<'a> Contraption<'a> {
-    fn new(bytes: &'a [u8]) -> Self {
-        let width = bytes
-            .iter()
-            .position(|b| *b == b'\n')
-            .expect("No newline in input!");
-        let height = (bytes.len() + 1) / (width + 1);
+    fn new(input: &'a str) -> Self {
+        let grid = StrGrid::from_input(input);
+        dbg!(&grid);
         Self {
-            grid: bytes,
-            width,
-            height,
-            grid_beams: vec![0; bytes.len()],
+            grid_beams: vec![0; grid.data.len()],
+            grid,
         }
     }
-    fn get(&mut self, x: usize, y: usize) -> (&u8, &mut u8) {
-        let cur = x + y * (self.width + 1);
-        (&self.grid[cur], &mut self.grid_beams[cur])
-    }
-    // moves (x,y) accroding to Direction.
-    // returns true if stepped out of bounds
-    fn step(&self, x: &mut usize, y: &mut usize, dir: Direction) -> bool {
-        match (dir, *x, *y) {
-            (Left, 0, _) => return true,
-            (Right, _, _) if *x == self.width - 1 => return true,
-            (Up, _, 0) => return true,
-            (Down, _, _) if *y == self.height - 1 => return true,
-            (Left, _, _) => *x -= 1,
-            (Right, _, _) => *x += 1,
-            (Up, _, _) => *y -= 1,
-            (Down, _, _) => *y += 1,
-        }
-        false
+    fn get(&mut self, pos: Position) -> (&u8, &mut u8) {
+        let cur = self.grid.cur(pos);
+        (&self.grid.data[cur], &mut self.grid_beams[cur])
     }
     fn reset_beams(&mut self) {
         self.grid_beams.fill(0);
     }
-    fn start_beam(&mut self, start: (usize, usize, Direction)) -> usize {
+    fn start_beam(&mut self, start: (Position, Direction)) -> usize {
         let mut energized = 0;
         let mut beams = Vec::with_capacity(16);
         beams.push(start);
-        while let Some((mut x, mut y, mut dir)) = beams.pop() {
-            let (ground, seen_beams) = self.get(x, y);
+        while let Some((mut pos, mut dir)) = beams.pop() {
+            let (ground, seen_beams) = self.get(pos);
             if *seen_beams & (dir as u8) != 0 {
                 // we already went here in this direction
                 continue;
@@ -87,22 +59,24 @@ impl<'a> Contraption<'a> {
                 (b'\\', Up) => dir = Left,
                 (b'-', Up) | (b'-', Down) => {
                     dir = Left;
-                    let (mut x2, mut y2, dir2) = (x, y, Right);
-                    if !self.step(&mut x2, &mut y2, dir2) {
-                        beams.push((x2, y2, dir2));
+                    let mut pos2 = pos;
+                    let dir2 = Right;
+                    if self.grid.step_mut(&mut pos2, dir2) {
+                        beams.push((pos2, dir2));
                     }
                 }
                 (b'|', Left) | (b'|', Right) => {
                     dir = Down;
-                    let (mut x2, mut y2, dir2) = (x, y, Up);
-                    if !self.step(&mut x2, &mut y2, dir2) {
-                        beams.push((x2, y2, dir2));
+                    let mut pos2 = pos;
+                    let dir2 = Up;
+                    if self.grid.step_mut(&mut pos2, dir2) {
+                        beams.push((pos2, dir2));
                     }
                 }
                 _ => {}
             }
-            if !self.step(&mut x, &mut y, dir) {
-                beams.push((x, y, dir));
+            if self.grid.step_mut(&mut pos, dir) {
+                beams.push((pos, dir));
             }
         }
         energized
@@ -110,18 +84,30 @@ impl<'a> Contraption<'a> {
 }
 
 fn run(input: &str) -> usize {
-    let mut contraption = Contraption::new(input.as_bytes());
+    let mut contraption = Contraption::new(input);
     let mut max = 0;
-    for x in 0..contraption.width {
-        max = max.max(contraption.start_beam((x, 0, Down)));
+    for x in 0..contraption.grid.width - 1 {
+        max = max.max(contraption.start_beam((Position { x, y: 0 }, Down)));
         contraption.reset_beams();
-        max = max.max(contraption.start_beam((x, contraption.height - 1, Up)));
+        max = max.max(contraption.start_beam((
+            Position {
+                x,
+                y: contraption.grid.height - 1,
+            },
+            Up,
+        )));
         contraption.reset_beams();
     }
-    for y in 0..contraption.height {
-        max = max.max(contraption.start_beam((0, y, Right)));
+    for y in 0..contraption.grid.height {
+        max = max.max(contraption.start_beam((Position { x: 0, y }, Right)));
         contraption.reset_beams();
-        max = max.max(contraption.start_beam((contraption.width - 1, y, Left)));
+        max = max.max(contraption.start_beam((
+            Position {
+                x: contraption.grid.width - 2,
+                y,
+            },
+            Left,
+        )));
         contraption.reset_beams();
     }
     max
@@ -143,7 +129,8 @@ mod tests {
 ..../.\\..
 .-.-/..|..
 .|....-|.\
-..//.|...."),
+..//.|....
+"),
             51
         )
     }
