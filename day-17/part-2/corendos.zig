1a928334efa8f90dbd1c69334affb5760baca7e3
Corendos <corentin.godeau37@gmail.com>
2023-12-19 15:46:05 +0100

    Fix Part 2

--- day-17/part-2/corendos.zig
+++ day-17/part-2/corendos.zig
@@ -3,30 +3,45 @@ const std = @import("std");
 var allocator: std.mem.Allocator = undefined;
 const stdout = std.io.getStdOut().writer(); //prepare stdout to write in
 
-const Grid = struct {
-    width: usize,
-    height: usize,
-    data: []u8,
+fn Grid(comptime T: type) type {
+    return struct {
+        width: usize,
+        height: usize,
+        data: []T,
 
-    pub inline fn at(self: Grid, x: usize, y: usize) u8 {
-        return self.data[y * self.width + x];
-    }
-
-    pub fn format(self: Grid, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
-        _ = fmt;
-        _ = options;
-        for (0..self.height) |y| {
-            for (0..self.width) |x| {
-                try writer.print("{}", .{self.data[y * self.width + x]});
-            }
-            try writer.writeByte('\n');
+        pub inline fn at(self: @This(), x: usize, y: usize) T {
+            return self.data[y * self.width + x];
         }
-    }
-};
+
+        pub inline fn atPtr(self: @This(), x: usize, y: usize) *T {
+            return &self.data[y * self.width + x];
+        }
+
+        pub fn format(self: @This(), comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+            _ = fmt;
+            _ = options;
+            for (0..self.height) |y| {
+                for (0..self.width) |x| {
+                    try writer.print("{}", .{self.data[y * self.width + x]});
+                }
+                try writer.writeByte('\n');
+            }
+        }
+    };
+}
 
 const Position = struct {
     x: usize,
     y: usize,
+
+    pub inline fn move(self: Position, direction: Direction, step: usize, width: usize, height: usize) ?Position {
+        return switch (direction) {
+            .up => if (self.y >= step) .{ .x = self.x, .y = self.y - step } else null,
+            .down => if (self.y + step < height) .{ .x = self.x, .y = self.y + step } else null,
+            .left => if (self.x >= step) .{ .x = self.x - step, .y = self.y } else null,
+            .right => if (self.x + step < width) .{ .x = self.x + step, .y = self.y } else null,
+        };
+    }
 };
 
 const Direction = enum {
@@ -42,7 +57,7 @@ const Vertex = struct {
     count: usize,
 };
 
-fn parseGrid(input: []const u8) !Grid {
+fn parseGrid(input: []const u8) !Grid(u8) {
     const width = std.mem.indexOfScalar(u8, input, '\n').?;
     const height = std.mem.count(u8, input, "\n") + 1;
 
@@ -54,169 +69,117 @@ fn parseGrid(input: []const u8) !Grid {
         i += 1;
     }
 
-    return Grid{ .width = width, .height = height, .data = data };
+    return Grid(u8){ .width = width, .height = height, .data = data };
 }
 
-fn getNeighbors(vertex: Vertex, grid: Grid) [3]?Vertex {
-    var result = [_]?Vertex{null} ** 3;
+const QueueContext = struct {};
 
-    switch (vertex.direction) {
-        .up => {
-            if (vertex.count >= 3 and vertex.position.x > 0) {
-                result[0] = Vertex{
-                    .position = .{ .x = vertex.position.x - 1, .y = vertex.position.y },
-                    .direction = .left,
-                    .count = 0,
-                };
-            }
+const QueueItem = struct {
+    cost: usize,
+    position: Position,
+    vertical: u8,
 
-            if (vertex.count >= 3 and vertex.position.x < grid.width - 1) {
-                result[1] = Vertex{
-                    .position = .{ .x = vertex.position.x + 1, .y = vertex.position.y },
-                    .direction = .right,
-                    .count = 0,
-                };
-            }
+    pub fn compare(a: QueueItem, b: QueueItem) std.math.Order {
+        const cost_order = std.math.order(a.cost, b.cost);
+        if (cost_order != .eq) return cost_order;
 
-            if (vertex.count < 9 and vertex.position.y > 0) {
-                result[2] = Vertex{
-                    .position = .{ .x = vertex.position.x, .y = vertex.position.y - 1 },
-                    .direction = .up,
-                    .count = vertex.count + 1,
-                };
-            }
-        },
-        .down => {
-            if (vertex.count >= 3 and vertex.position.x > 0) {
-                result[0] = Vertex{
-                    .position = .{ .x = vertex.position.x - 1, .y = vertex.position.y },
-                    .direction = .left,
-                    .count = 0,
-                };
-            }
+        const position_x_order = std.math.order(a.position.x, b.position.x);
+        if (position_x_order != .eq) return position_x_order;
 
-            if (vertex.count >= 3 and vertex.position.x < grid.width - 1) {
-                result[1] = Vertex{
-                    .position = .{ .x = vertex.position.x + 1, .y = vertex.position.y },
-                    .direction = .right,
-                    .count = 0,
-                };
-            }
+        const position_y_order = std.math.order(a.position.y, b.position.y);
+        if (position_y_order != .eq) return position_y_order;
 
-            if (vertex.count < 9 and vertex.position.y < grid.height - 1) {
-                result[2] = Vertex{
-                    .position = .{ .x = vertex.position.x, .y = vertex.position.y + 1 },
-                    .direction = .down,
-                    .count = vertex.count + 1,
-                };
-            }
-        },
-        .left => {
-            if (vertex.count >= 3 and vertex.position.y > 0) {
-                result[0] = Vertex{
-                    .position = .{ .x = vertex.position.x, .y = vertex.position.y - 1 },
-                    .direction = .up,
-                    .count = 0,
-                };
-            }
-
-            if (vertex.count >= 3 and vertex.position.y < grid.height - 1) {
-                result[1] = Vertex{
-                    .position = .{ .x = vertex.position.x, .y = vertex.position.y + 1 },
-                    .direction = .down,
-                    .count = 0,
-                };
-            }
-
-            if (vertex.count < 9 and vertex.position.x > 0) {
-                result[2] = Vertex{
-                    .position = .{ .x = vertex.position.x - 1, .y = vertex.position.y },
-                    .direction = .left,
-                    .count = vertex.count + 1,
-                };
-            }
-        },
-        .right => {
-            if (vertex.count >= 3 and vertex.position.y > 0) {
-                result[0] = Vertex{
-                    .position = .{ .x = vertex.position.x, .y = vertex.position.y - 1 },
-                    .direction = .up,
-                    .count = 0,
-                };
-            }
-
-            if (vertex.count >= 3 and vertex.position.y < grid.height - 1) {
-                result[1] = Vertex{
-                    .position = .{ .x = vertex.position.x, .y = vertex.position.y + 1 },
-                    .direction = .down,
-                    .count = 0,
-                };
-            }
-
-            if (vertex.count < 9 and vertex.position.x < grid.width - 1) {
-                result[2] = Vertex{
-                    .position = .{ .x = vertex.position.x + 1, .y = vertex.position.y },
-                    .direction = .right,
-                    .count = vertex.count + 1,
-                };
-            }
-        },
-    }
-
-    return result;
-}
-const QueueContext = struct {
-    distance: *const std.AutoHashMap(Vertex, usize),
-
-    pub fn compare(self: QueueContext, a: Vertex, b: Vertex) std.math.Order {
-        const a_priority = self.distance.get(a) orelse std.math.maxInt(usize);
-        const b_priority = self.distance.get(b) orelse std.math.maxInt(usize);
-
-        return std.math.order(a_priority, b_priority);
+        return std.math.order(a.vertical, b.vertical);
     }
 };
 
 fn run(input: [:0]const u8) !i64 {
     const grid = try parseGrid(input);
 
-    var distance = std.AutoHashMap(Vertex, usize).init(allocator);
-    var queue = std.PriorityQueue(Vertex, QueueContext, QueueContext.compare).init(allocator, .{ .distance = &distance });
+    var distance = Grid([2]usize){
+        .width = grid.width,
+        .height = grid.height,
+        .data = b: {
+            const data = try allocator.alloc([2]usize, grid.width * grid.height);
+            @memset(data, [2]usize{ std.math.maxInt(usize), std.math.maxInt(usize) });
+            break :b data;
+        },
+    };
 
-    const source1 = Vertex{ .position = .{ .x = 0, .y = 0 }, .direction = .right, .count = 0 };
-    try distance.put(source1, 0);
-    try queue.add(source1);
-    const source2 = Vertex{ .position = .{ .x = 0, .y = 0 }, .direction = .down, .count = 0 };
-    try distance.put(source2, 0);
-    try queue.add(source2);
+    var queue = std.PriorityQueue(QueueItem, void, (struct {
+        pub fn compare(ctx: void, a: QueueItem, b: QueueItem) std.math.Order {
+            _ = ctx;
+            return QueueItem.compare(a, b);
+        }
+    }).compare).init(allocator, {});
+
+    try queue.add(.{ .cost = 0, .position = .{ .x = 0, .y = 0 }, .vertical = 0 });
+    try queue.add(.{ .cost = 0, .position = .{ .x = 0, .y = 0 }, .vertical = 1 });
 
     while (queue.peek()) |u| {
         _ = queue.remove();
         //std.debug.print("Exploring node {}:{} with direction {s} and count {}\n", .{ u.position.x, u.position.y, @tagName(u.direction), u.count });
-        const d = distance.get(u).?;
-        if (u.position.x == grid.width - 1 and u.position.y == grid.height - 1 and u.count >= 3) {
-            return @intCast(d);
+        if (u.position.x == grid.width - 1 and u.position.y == grid.height - 1) {
+            return @intCast(u.cost);
         }
 
-        const neighbors = getNeighbors(u, grid);
-        for (neighbors) |maybe_n| if (maybe_n) |n| {
-            const alt = d + @as(usize, grid.at(n.position.x, n.position.y));
-            if (alt < distance.get(n) orelse std.math.maxInt(usize)) {
-                try distance.put(n, alt);
-                try queue.add(n);
+        const new_vertical = 1 - u.vertical;
+        if (u.vertical == 1) {
+            var cost_up = u.cost;
+            var cost_down = u.cost;
+            for (1..11) |steps| {
+                if (u.position.move(.up, steps, grid.width, grid.height)) |pos_up| {
+                    cost_up += grid.at(pos_up.x, pos_up.y);
+                    if (steps >= 4) {
+                        const old_cost = &distance.atPtr(pos_up.x, pos_up.y).*[new_vertical];
+                        if (cost_up < old_cost.*) {
+                            old_cost.* = cost_up;
+                            try queue.add(.{ .cost = cost_up, .position = pos_up, .vertical = new_vertical });
+                        }
+                    }
+                }
+
+                if (u.position.move(.down, steps, grid.width, grid.height)) |pos_down| {
+                    cost_down += grid.at(pos_down.x, pos_down.y);
+                    if (steps >= 4) {
+                        const old_cost = &distance.atPtr(pos_down.x, pos_down.y).*[new_vertical];
+                        if (cost_down < old_cost.*) {
+                            old_cost.* = cost_down;
+                            try queue.add(.{ .cost = cost_down, .position = pos_down, .vertical = new_vertical });
+                        }
+                    }
+                }
             }
-        };
-    }
+        } else {
+            var cost_left = u.cost;
+            var cost_right = u.cost;
+            for (1..11) |steps| {
+                if (u.position.move(.left, steps, grid.width, grid.height)) |pos_left| {
+                    cost_left += grid.at(pos_left.x, pos_left.y);
+                    if (steps >= 4) {
+                        const old_cost = &distance.atPtr(pos_left.x, pos_left.y).*[new_vertical];
+                        if (cost_left < old_cost.*) {
+                            old_cost.* = cost_left;
+                            try queue.add(.{ .cost = cost_left, .position = pos_left, .vertical = new_vertical });
+                        }
+                    }
+                }
 
-    var result: i64 = std.math.maxInt(i64);
-    var it = distance.iterator();
-    while (it.next()) |entry| {
-        if (entry.key_ptr.position.x == grid.width - 1 and entry.key_ptr.position.y == grid.height - 1 and entry.key_ptr.count >= 3) {
-            std.debug.print("Distance for {s} and count {}: {}\n", .{ @tagName(entry.key_ptr.direction), entry.key_ptr.count, entry.value_ptr.* });
-            result = @min(result, @as(i64, @intCast(entry.value_ptr.*)));
+                if (u.position.move(.right, steps, grid.width, grid.height)) |pos_right| {
+                    cost_right += grid.at(pos_right.x, pos_right.y);
+                    if (steps >= 4) {
+                        const old_cost = &distance.atPtr(pos_right.x, pos_right.y).*[new_vertical];
+                        if (cost_right < old_cost.*) {
+                            old_cost.* = cost_right;
+                            try queue.add(.{ .cost = cost_right, .position = pos_right, .vertical = new_vertical });
+                        }
+                    }
+                }
+            }
         }
     }
 
-    return result;
+    return 0;
 }
 
 pub fn main() !void {
