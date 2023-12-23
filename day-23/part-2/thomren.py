440296c97596ee6de8681ab29f26ca914831fa70
Thomas Renault <thomas.renault56@gmail.com>
2023-12-23 13:04:47 +0100

    speed up

--- day-23/part-2/thomren.py
+++ day-23/part-2/thomren.py
@@ -13,29 +13,31 @@ class ThomrenSubmission(SubmissionPy):
         start = (0, next(i for i, c in enumerate(grid[0]) if c == "."))
         end = (len(grid) - 1, next(i for i, c in enumerate(grid[-1]) if c == "."))
 
-        @cache
-        def find_next_intersection(pos, direction):
-            dist = 0
-            x, y = pos
-            dx, dy = direction
-            neighbors = set(get_neighbors(grid, pos)) - {(x - dx, y - dy)}
-            while len(neighbors) == 1:
-                neighbor = next(n for n in neighbors if n != (x - dx, y - dy))
-                dx, dy = (neighbor[0] - x, neighbor[1] - y)
-                x, y = neighbor
-                dist += 1
-                neighbors = set(get_neighbors(grid, (x, y))) - {(x - dx, y - dy)}
-            return (x, y), dist + 1
+        # build condensed graph
+        stack = [start]
+        visited = set()
+        graph = defaultdict(set)
+        while stack:
+            pos = stack.pop()
+            if pos == end or pos in visited:
+                continue
+            visited.add(pos)
 
+            for neighbor in get_neighbors(grid, pos):
+                (nx, ny), nd = find_next_intersection(
+                    grid, neighbor, (neighbor[0] - pos[0], neighbor[1] - pos[1])
+                )
+                graph[pos].add(((nx, ny), nd))
+                stack.append((nx, ny))
+
+        # find longest path
         stack = [(start, 0)]
         visited = set()
         max_dist = 0
         while stack:
             pos, dist = stack.pop()
-            # print(pos, dist)
             if pos == end:
                 max_dist = max(max_dist, dist)
-                # print(max_dist)
                 continue
             elif dist == -1:
                 visited.remove(pos)
@@ -47,15 +49,25 @@ class ThomrenSubmission(SubmissionPy):
             # will backtrack once all the paths from pos have been explored
             stack.append((pos, -1))
 
-            for neighbor in get_neighbors(grid, pos):
-                (nx, ny), nd = find_next_intersection(
-                    neighbor, (neighbor[0] - pos[0], neighbor[1] - pos[1])
-                )
-                if (nx, ny) not in visited:
-                    stack.append(((nx, ny), dist + nd))
-            # print(stack)
+            for neighbor, d in graph[pos]:
+                if neighbor not in visited:
+                    stack.append((neighbor, dist + d))
 
         return max_dist
+
+
+def find_next_intersection(grid, pos, direction):
+    dist = 0
+    x, y = pos
+    dx, dy = direction
+    neighbors = set(get_neighbors(grid, pos)) - {(x - dx, y - dy)}
+    while len(neighbors) == 1:
+        neighbor = next(n for n in neighbors if n != (x - dx, y - dy))
+        dx, dy = (neighbor[0] - x, neighbor[1] - y)
+        x, y = neighbor
+        dist += 1
+        neighbors = set(get_neighbors(grid, (x, y))) - {(x - dx, y - dy)}
+    return (x, y), dist + 1
 
 
 def get_neighbors(grid: list[list[str]], pos: tuple[int, int]) -> list[tuple[int, int]]:
