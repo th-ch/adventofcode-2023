f12d25fd1cbe233394a321d37873170da850e427
skasch <romain.mondoncancel@gmail.com>
2023-12-27 12:06:37 +0100

    Optimize day 24
    Use long double over Frac, use int64 over int128

--- day-24/part-1/skasch.cpp
+++ day-24/part-1/skasch.cpp
@@ -1,89 +1,17 @@
 #include <chrono>
+#include <cstdint>
 #include <iostream>
 #include <span>
 #include <sstream>
 #include <string>
 
-static constexpr __int128_t kMinTestArea = 200000000000000;
-static constexpr __int128_t kMaxTestArea = 400000000000000;
+static constexpr std::int64_t kMinTestArea = 200000000000000;
+static constexpr std::int64_t kMaxTestArea = 400000000000000;
 static constexpr int kNHails = 300;
 
-__int128_t gcd(__int128_t a, __int128_t b) {
-  if (b < 0) b = -b;
-  if (a < 0) a = -a;
-  if (a < b) std::swap(a, b);
-  if (b == 0) return a;
-  __int128_t r;
-  while (a % b > 0) {
-    r = a % b;
-    a = b;
-    b = r;
-  }
-  return b;
-}
-
-class Frac {
- public:
-  Frac(__int128_t n) : num_(n), den_(1) {}
-  Frac() : Frac(0) {}
-  Frac(__int128_t n, __int128_t d) {
-    if (d < 0) {
-      n = -n;
-      d = -d;
-    }
-    __int128_t g = gcd(n, d);
-    num_ = n / g;
-    den_ = d / g;
-  }
-
-  __int128_t n() const { return num_; }
-  __int128_t d() const { return den_; }
-
-  Frac operator+(const Frac& o) const {
-    __int128_t g = gcd(d(), o.d());
-    __int128_t d1 = d() / g;
-    __int128_t d2 = o.d() / g;
-    return Frac(n() * d2 + d1 * o.n(), g * d1 * d2);
-  }
-  Frac operator-() const { return Frac(-n(), d()); }
-  Frac operator-(const Frac& o) const { return *this + (-o); }
-
-  Frac operator*(const Frac& o) const {
-    __int128_t g1 = gcd(n(), o.d());
-    __int128_t g2 = gcd(o.n(), d());
-    return Frac((n() / g1) * (o.n() / g2), (d() / g2) * (o.d() / g1));
-  }
-  Frac operator/(__int128_t k) const {
-    __int128_t g = gcd(n(), k);
-    return Frac(n() / g, d() * (k / g));
-  }
-  friend Frac operator/(__int128_t k, const Frac& o) {
-    __int128_t g = gcd(k, o.n());
-    return Frac((k / g) * o.d(), o.n() / g);
-  }
-  Frac operator/(const Frac& o) const { return *this * (1 / o); }
-
-  Frac operator*(__int128_t k) {
-    __int128_t g = gcd(k, d());
-    return Frac((k / g) * n(), d() / g);
-  }
-  friend Frac operator*(__int128_t k, const Frac& o) { return o * k; }
-
-  bool operator==(const Frac& o) const { return n() == o.n() && d() == o.d(); }
-  auto operator<=>(const Frac& o) const { return n() * o.d() <=> d() * o.n(); }
-  auto operator<=>(__int128_t k) const { return n() <=> d() * k; }
-  friend auto operator<=>(__int128_t k, const Frac& o) {
-    return k * o.d() <=> o.n();
-  }
-
- private:
-  __int128_t num_;
-  __int128_t den_;
-};
-
 struct Vec {
-  Frac x;
-  Frac y;
+  long double x;
+  long double y;
 
   bool InTestArea() {
     return kMinTestArea <= x && x <= kMaxTestArea && kMinTestArea <= y &&
@@ -91,7 +19,9 @@ struct Vec {
   }
 
   Vec operator+(const Vec& o) const { return {x + o.x, y + o.y}; }
-  friend Vec operator*(Frac k, const Vec& v) { return {k * v.x, k * v.y}; }
+  friend Vec operator*(long double k, const Vec& v) {
+    return {k * v.x, k * v.y};
+  }
 };
 
 struct Hail {
@@ -99,12 +29,13 @@ struct Hail {
   Vec v;
 
   std::optional<Vec> Intersect(const Hail& o) const {
-    Frac delta = v.x * o.v.y - v.y * o.v.x;
+    long double delta = v.x * o.v.y - v.y * o.v.x;
     if (delta == 0) {
       return std::nullopt;
     }
-    Frac a = o.v.y / delta * (o.p.x - p.x) - o.v.x / delta * (o.p.y - p.y);
-    Frac b = v.y / delta * (o.p.x - p.x) - v.x / delta * (o.p.y - p.y);
+    long double a =
+        o.v.y / delta * (o.p.x - p.x) - o.v.x / delta * (o.p.y - p.y);
+    long double b = v.y / delta * (o.p.x - p.x) - v.x / delta * (o.p.y - p.y);
     if (a < 0.0 || b < 0.0) {
       return std::nullopt;
     }
