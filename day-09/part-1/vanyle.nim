9056ce12d8802d37899b7be3b6edac4dea8c2c7c
Antoine Delègue <delegue.antoine@gmail.com>
2023-12-28 17:36:01 +0100

    day 9

---
+++ day-09/part-1/vanyle.nim
@@ -0,0 +1,42 @@
+from times import cpuTime
+from os import paramStr
+
+import ../../lib/nim/vanyle/speed_utils
+
+proc isAllZero(v: seq[int]): bool =
+    for i in v:
+        if i != 0: return false
+    return true
+
+proc extrapolate(v: seq[int]): int =
+    var exStack: seq[seq[int]] = newSeqOfCap[seq[int]](v.len)
+    exStack.add v
+    while (not isAllZero exStack[^1]):
+        var p: seq[int]
+        for i in 1..<exStack[^1].len:
+            p.add(exStack[^1][i] - exStack[^1][i-1])
+
+        exStack.add p
+
+    var nextValue = 0
+    for i in countdown(exStack.len - 1, 0):
+        nextValue += exStack[i][^1]
+
+    return nextValue
+
+proc run(s: string): string =
+    var res = 0
+    for l in s.fastSplit('\n'):
+        if l.len == 0: continue
+        var i = ints(l)
+        res += extrapolate(i)
+
+    return $res
+
+var input: string = paramStr(1)
+
+var t0 = cpuTime()
+var output = run(input)
+
+echo "_duration:", (cpuTime() - t0) * 1000
+echo output
